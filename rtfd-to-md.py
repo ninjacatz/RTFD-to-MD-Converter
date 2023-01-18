72d2d167b861f192b01939a19ce99d46a3b5668e
ninjacatz <talonak@protonmail.com>
2023-01-18 23:07:56 +0000

    Improved algorthm and fixed bugs

--- rtfd-to-md.py
+++ rtfd-to-md.py
@@ -7,7 +7,7 @@
 
 # ------User-Defined Variables-------
 # Directory to begin process:
-start_directory = r'/Users/ninjacats/Documents/Notes/Journal/000-Encoding/project/test'
+start_directory = r''
 # Extra features
 do_image_convert = False
 image_convert_extension = '.jpg'
@@ -15,7 +15,6 @@ do_video_convert = False
 video_convert_extension = '.mp4'
 
 # ------Other Variables------
-rtf_extensions = ('.rtf', '.RTF')
 pandoc_command = [
     'pandoc',
     'source file (automatically generated)',
@@ -27,14 +26,14 @@ pandoc_command = [
     'output file (automatically generated)'
 ]
 
-apple_image_extensions = ('.heic', '.HEIC', '.heics', '.HEICS', '.heif', '.HEIF', '.heif', '.heifs', '.hevc', '.HEVC')
+apple_image_extensions = ('.heic', '.heif')
 imagemagick_command = [
     'magick',
     'source file (automatically generated)',
     'output file (automatically generated)'
 ]
 
-apple_video_extensions = ('.mov', '.MOV')
+apple_video_extensions = ('.mov', '.hevc')
 ffmpeg_command = [
     'ffmpeg',
     '-i',
@@ -47,70 +46,65 @@ import subprocess
 import os
 
 def main():
-    recursiveSearchRtf(start_directory)
-    if do_image_convert or do_video_convert:
-        recursiveSearchImageAndVideo(start_directory)
+    recursiveSearch(start_directory)
     print('Done!')
 
-def recursiveSearchRtf(directory):
+def recursiveSearch(directory):
     for file in os.listdir(directory):
         file_path = os.path.join(directory, file)
 
-        if file.endswith(rtf_extensions) and 'rtfd' in file_path:
-            print(file_path)
-            doRtfConversion(file_path)
-            fixMdAttachments(file_path.split('.')[0] + '/' + file_path.split('.')[1].split('/')[1] + '.md')
+        if os.path.isdir(file_path) and file_path.lower().endswith('.rtfd'):
+            # .rtf file must be converted first because fixing .md attachments requires a converted .md file
+            for rtfd_file in os.listdir(file_path):
+                rtfd_file_path = os.path.join(file_path, rtfd_file)
+                md_path = os.path.join(os.path.dirname(rtfd_file_path), 'TXT.md')
+
+                if rtfd_file.lower().endswith('.rtf'):
+                    print('RTF TO CONVERT: ', rtfd_file_path)
+                    doRtfConversion(rtfd_file_path)
+                    # BUG!!! because md_path is the 'TXT.md' file only, it ignores any other .rtf files
+                    fixMdAttachments(md_path)
+
+            # now that the .rtf file is converted, convert images and videos
+            if do_image_convert or do_video_convert:
+                for rtfd_file in os.listdir(file_path):
+                    rtfd_file_path = os.path.join(file_path, rtfd_file)
+                    md_path = os.path.join(os.path.dirname(rtfd_file), 'TXT.md')
+
+                    if rtfd_file.lower().endswith(apple_image_extensions) and do_image_convert:
+                        print('IMAGE TO CONVERT: ', rtfd_file_path)
+                        doImageConversion(rtfd_file_path, rtfd_file)
+                        fixImageAttachments(md_path, rtfd_file)
+
+                    if rtfd_file.lower().endswith(apple_video_extensions) and do_video_convert:
+                        print("VIDEO TO CONVERT: ", rtfd_file_path)
+                        doVideoConversion(rtfd_file_path, rtfd_file)
+                        fixVideoAttachments(md_path, rtfd_file)
+
+            # rename .rtfd directory to remove '.rtfd'
+            os.rename(file_path, file_path.replace('.rtfd', ''))
 
         elif os.path.isdir(file_path):
-            recursiveSearchRtf(file_path)
-
-def recursiveSearchImageAndVideo(directory):
-    for file in os.listdir(directory):
-        file_path = os.path.join(directory, file)
-            
-        # must only convert in former '.rtfd' directories
-        # this means directory will have 'TXT.md'
-        former_rtfd_directory = False
-        for file2 in os.listdir(directory):
-            if file2 == 'TXT.md':
-                former_rtfd_directory = True
-
-        if former_rtfd_directory:
-            if file.endswith(apple_image_extensions) and do_image_convert:
-                print(file_path)
-                doImageConversion(file_path, file)
-                fixImageAttachments(os.path.dirname(file_path) + '/TXT.md', file)
-
-            elif file.endswith(apple_video_extensions) and do_video_convert:
-                print(file_path)
-                doVideoConversion(file_path, file)
-                fixVideoAttachments(os.path.dirname(file_path) + '/TXT.md', file)
-
-        if os.path.isdir(file_path):
-            recursiveSearchImageAndVideo(file_path)
+            recursiveSearch(file_path)
 
 def doRtfConversion(rtf_path):
+    # BUG!!! what if .rtf file is not named 'TXT.rtf'? (example: there are multiple .rtf files in the .rtfd directory, or the user manually renamed the file)
     os.chdir(os.path.dirname(rtf_path))
     # set source file in command
     pandoc_command[1] = rtf_path
     # set output file in command
-    # FIX!!!!! deliberately named TXT.md for recursiveSearchImageAndVideo function
-    pandoc_command[7] = os.path.dirname(rtf_path) + '/TXT.md'
-    print(pandoc_command[7])
+    pandoc_command[7] = os.path.join(os.path.dirname(rtf_path), 'TXT.md')
     # perform conversion to markdown using pandoc
     subprocess.run(pandoc_command)
     # remove rtf file
     os.remove(rtf_path)
-    # remove '.rtfd' from enclosing folder
-    os.rename(os.path.dirname(rtf_path), os.path.dirname(rtf_path).split('.')[0])
 
 def fixMdAttachments(md_path):
     with open(md_path, 'r') as f:
         data = f.readlines()
 
     for (index, line) in enumerate(data):
-        # '¬' denotes a file attachment in rtfd when
-        # converted to markdown
+        # '¬' denotes a file attachment in .rtf when converted to .md
         #
         # CONVERT FROM: 
         # grand haven.jpg ¬   
@@ -146,11 +140,9 @@ def fixImageAttachments(md_path, image):
 
     # Example:
     # CONVERT FROM: 
-    # ![ATTACHMENT: grand haven.HIEC](grand%20haven.HEIC) 
+    # ![grand haven.HEIC](grand%20haven.HEIC) 
     # CONVERT TO:   
-    # ![ATTACHMENT: grand haven.jpg](grand%20haven.jpg)
-    # FIX!!! Must be FULL file name
-    # FIX!!!! this so it works when there is '%20'
+    # ![grand haven.jpg](grand%20haven.jpg)
     for (index, line) in enumerate(data):
         updated_line = line.replace(image, image.split('.')[0] + image_convert_extension)
         # in cases where '%20' is used
@@ -167,7 +159,8 @@ def doVideoConversion(video_path, video):
     # set renamed video as source file
     ffmpeg_command[2] = os.path.join(os.path.dirname(video_path), 'renamed-' + video)
     # set original video name as destination
-    ffmpeg_command[3] = video_path.split('.')[0] + video_convert_extension
+    ffmpeg_command[3] = os.path.join(os.path.dirname(video_path), video.split('.')[0] + video_convert_extension)
+    # perform encoding with imagemagick
     # perform encoding with ffmpeg
     subprocess.run(ffmpeg_command)
     # remove renamed original video
@@ -179,9 +172,9 @@ def fixVideoAttachments(md_path, video):
 
     # Example:
     # CONVERT FROM: 
-    # ![ATTACHMENT: grand haven.MOV](grand%20haven.MOV) 
+    # ![grand haven.MOV](grand%20haven.MOV) 
     # CONVERT TO:   
-    # ![ATTACHMENT: grand haven.mp4](grand%20haven.mp4)
+    # ![grand haven.mp4](grand%20haven.mp4)
     for (index, line) in enumerate(data):
         updated_line = line.replace(video, video.split('.')[0] + video_convert_extension)
         # in cases where '%20' is used
